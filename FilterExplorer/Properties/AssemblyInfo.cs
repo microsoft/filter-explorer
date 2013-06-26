e89c17a16f5df75463aaa726b500b5d60bb26d46
Tomi Paananen <tomi.paananen@nokia.com>
2013-06-26 13:40:48 +0300

    Version 1.0.

--- FilterExplorer/Properties/AssemblyInfo.cs
+++ FilterExplorer/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageProcessingApp")]
+[assembly: AssemblyTitle("Filter Explorer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImageProcessingApp")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Filter Explorer")]
+[assembly: AssemblyCopyright("Copyright © 2013 Nokia Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
